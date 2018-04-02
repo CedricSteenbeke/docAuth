ac5d7ddac39944118cce9b4fd4b2be0cbad58813
cedric <cedric.steenbeke@gmail.com>
2018-04-02 15:33:09 +0200

    Added story 3 functionality

--- contracts/DocAuth.sol
+++ contracts/DocAuth.sol
@@ -11,12 +11,14 @@ contract DocAuth {
     
     mapping (bytes32 => DocMetadata) registeredDocuments;
     mapping (bytes32 => address)     documentToOwner;
+    mapping (address => bytes32[])   ownerToDocument;
+    mapping (address => uint)        ownerDocumentCount;
     
-
     function registerDocument(bytes32 _documentHash, bytes _title,  bytes _author, bytes _email, uint _dateWritten) public returns (bool){
         require(documentToOwner[_documentHash] == 0);
         registeredDocuments[_documentHash] = DocMetadata(_title, _author, _email, _dateWritten);
         documentToOwner[_documentHash] = msg.sender ;
+        ownerDocumentCount[msg.sender] ++;
         return true;
     }
     
@@ -33,4 +35,11 @@ contract DocAuth {
          return (dmd.title, dmd.author, dmd.email, dmd.dateWritten);
     }
 
+    function getOwnerDocumentCount(address _docOwner) public view returns(uint){
+        return ownerDocumentCount[_docOwner];
+    }
+    
+    function getDocumentsForOwner(address _docOwner, uint _documentId) public view returns(bytes32){
+        return ownerToDocument[_docOwner][_documentId];
+    }
 }
